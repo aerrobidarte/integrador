6ebda12ee98fd862f13511cff7e08869c8a28189
aerrobidarte <aerrobidarte@gmail.com>
2022-09-05 21:31:19 -0300

    mod register

--- src/controllers/usersController.js
+++ src/controllers/usersController.js
@@ -6,6 +6,34 @@ let usersController = {
     login: (req, res) => {
         return res.render("./login.ejs");
       },
+    register: (req, res) => {
+        res.render("./register.ejs");
+      },
+      registerWrite: (req, res) => {
+        let errores = validationResult(req);
+    
+        if (errores.errors.length > 0) {
+          res.render("./register.ejs", {
+            errores: errores.mapped(),
+            oldData: req.body,
+          });
+        } else {
+          let key = bcryptjs.hashSync(req.body.password, 5);
+          let newUser = {
+            name: req.body.name,
+            email: req.body.email,
+            password: key,
+            remember_token: req.body.remember,
+            rol: req.body.userType,
+          };
+    
+          db.User.create(newUser)
+            .then(() => {
+              return res.redirect(`./login/`);
+            })
+            .catch((error) => res.send(error));
+        }
+      },
     
 }
 module.exports=usersController;
--- src/routes/usersRoute.js
+++ src/routes/usersRoute.js
@@ -6,15 +6,45 @@ let path = require('path');
 const db = require('../database/models');
 const guest = require('../middlewares/guest');
 const auth = require('../middlewares/auth');
+const {body} = require('express-validator');
+
+const validateUserRegister = [
+    body('name').notEmpty().withMessage('Por favor ingrese un nombre de usuario'),
+    body('name').custom((value, {req}) => {
+      return new Promise((resolve, reject) => {
+        db.User.findOne({where:{name:req.body.name}}).then(function(user){
+          
+          if(Boolean(user)) {
+            reject(new Error('El usuario ya estgá siendo usado por otra persona, por favor elegí uno distinto.'))
+          }
+          resolve(true)
+        }).catch(err => {console.log(err); reject(new Error('Error en el servidor'))});
+      });
+    }), 
+      body('email').isEmail().withMessage('Por favor ingresa un email valido'),
+      body('email').custom((value, {req}) => {
+          return new Promise((resolve, reject) => {
+            db.User.findOne({where:{email:req.body.email}}).then(function( user){
+              
+              if(Boolean(user)) {
+                reject(new Error('Ese email ya está registrado'))
+              }
+              resolve(true)
+            }).catch(err => {console.log(err); reject(new Error('Error en el servidor'))});
+          });
+        }), 
+      body('password').notEmpty().withMessage('Por favor ingrese una contraseña'),
+      body('password').isLength({min:8}).withMessage('Tu contraseña debe al menos tener 8 caracteres')
+  ];
 
 router.get('/users/login',guest, userController.login);
-// router.get('/register/:email', userController.askRegister);
-// router.get('/register',guest, userController.register);
+//router.get('/register/:email', userController.askRegister);
+router.get('/users/register',guest, userController.register);
 // router.get('/edit/:id',auth, userController.edit);
 // router.get('/profile',auth,userController.profile);
 // router.get('/logout', userController.logout);
 // router.post('/login', validateUserLogin, userController.loginProcess);
-// router.post('/register', uploadFile.single('image'), validateUserRegister, userController.registerWrite);
+router.post('/register', validateUserRegister, userController.registerWrite);
 // router.put('/update/:id',auth, uploadFile.single('image'), validateUserUpdate, userController.update);
 // router.get('*', userController.notFound);
 
